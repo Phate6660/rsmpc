e558293e01c54f83c046374266f7c24693c3196c
Ash <valleyknight@protonmail.com>
2020-06-15 19:15:48 -0400

    add db playtime to stats

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,25 @@ fn main() {
         let arts = stats.artists;
         let albs = stats.albums;
         let sngs = stats.songs;
-        println!("Songs: {}\nAlbums: {}\nArtists: {}", sngs, albs, arts);
+        let time = stats.db_playtime.num_seconds();
+        let pltm: String = if time > 86400 {
+            let t = time / 60 / 60 / 24;
+            let pltm = t.to_string() + "d";
+            pltm
+        } else if time > 3600 {
+            let t = time / 60 / 60;
+            let pltm = t.to_string() + "h";
+            pltm
+        } else if time > 60 {
+            let t = time / 60;
+            let pltm = t.to_string() + "m";
+            pltm
+        } else {
+            println!("Could not calculate DB Playtime.");
+            let pltm = "N/A".to_string();
+            pltm
+        };
+        println!("Songs: {}\nAlbums: {}\nArtists: {}\nDB Playtime: {}", sngs, albs, arts, pltm);
     } else if matches.is_present("status") {
         let status: Status = c.status().unwrap();
         let volume = status.volume;
