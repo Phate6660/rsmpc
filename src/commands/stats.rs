c7fe4ffb204ceed81136940d119255a7bd83bfd3
Ash <valleyknight@protonmail.com>
2020-06-15 22:16:55 -0400

    remove extraneous code

--- src/commands/stats.rs
+++ src/commands/stats.rs
@@ -7,20 +7,16 @@ pub fn obtain_stats(stats: Stats) {
     let time = stats.db_playtime.num_seconds();
     let pltm: String = if time > 86400 {
         let t = time / 60 / 60 / 24;
-        let pltm = t.to_string() + "d";
-        pltm
+        t.to_string() + "d"
     } else if time > 3600 {
         let t = time / 60 / 60;
-        let pltm = t.to_string() + "h";
-        pltm
+        t.to_string() + "h"
     } else if time > 60 {
         let t = time / 60;
-        let pltm = t.to_string() + "m";
-        pltm
+        t.to_string() + "m"
     } else {
         println!("Could not calculate DB Playtime.");
-        let pltm = "N/A".to_string();
-        pltm
+        "N/A".to_string()
     };
     println!("Songs:       {}\nAlbums:      {}\nArtists:     {}\nDB Playtime: {}", sngs, albs, arts, pltm);
 }
